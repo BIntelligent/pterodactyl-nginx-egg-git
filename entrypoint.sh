f2f7611266c7219c0fcf0dab694d5f44cd37ef39
BIntelligent <65684589+BIntelligent@users.noreply.github.com>
2024-07-11 23:14:58 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/container
-
+apt install -y git
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
